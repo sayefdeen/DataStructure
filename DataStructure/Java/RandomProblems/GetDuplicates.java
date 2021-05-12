990af10ea4a812e18928e033912a6a91caaae374
Sayefdeen <sayefdeen.alrawad@gmail.com>
2021-05-12 19:11:14 +0300

    duplicate using hashtable

--- DataStructure/Java/RandomProblems/GetDuplicates.java
+++ DataStructure/Java/RandomProblems/GetDuplicates.java
@@ -4,6 +4,7 @@ package RandomProblems;
 // Get the duplicates values inside an array with O(1) time complexity
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 
 public class GetDuplicates {
@@ -16,6 +17,22 @@ public class GetDuplicates {
                 list.set(j,-list.get(j));
             }else{
                 newList.add(j);
+            }
+        }
+        return newList;
+    }
+
+    static List<Integer> getDuplicatesUsingHash(List<Integer> list){
+        List<Integer> newList =new ArrayList<>();
+        Hashtable<Integer,Integer> hashtable =new Hashtable<>();
+        for (int j:list ) {
+            int counter = 0;
+            if(hashtable.get(j) != null){
+                counter = hashtable.get(j);
+            }
+            hashtable.put(j,++counter);
+            if(hashtable.get(j) >1){
+                list.add(j);
             }
         }
         return newList;
